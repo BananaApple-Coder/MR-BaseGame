8ba76865b38e7a7f7df8d51ae3483d4a3846b8fa
BananaAppleCoder <mohapatra9@gmail.com>
2021-08-12 21:03:35 -0500

    finish

--- js/Game.js
+++ js/Game.js
@@ -15,12 +15,42 @@ class Game {
     });
   }
 
-  start(){
+  async start(){
     if(gameState === 0){
       player = new Player();
-      player.getCount();
+      let playerCountRef = await database.ref('playerCount').once("value")
+      if (playerCountRef.exists()){
+        playerCount = playerCountRef.val()
+        player.getCount();
+      }
       form = new Form()
       form.display();
     }
   }
-}
+
+  play(){
+    form.hide()
+    textSize(30)
+    text("Go!", 120, 100)
+    Player.getPlayerInfo()
+
+    if (allPlayers !== undefined){
+      let disp_Position = 130
+      for (let plr in allPlayers){
+        if (plr === "player" + player.index){
+          fill("green")
+        } else{
+          fill("red")
+        }
+        disp_Position += 20
+      textSize(15)
+      text(allPlayers[plr].name + "Distance: " + allPlayers[plr].distance, 120, disp_Position)
+      }
+    }
+  
+
+  if (keyIsDown(UP_ARROW) && player.index !== null){
+    player.distance += 50
+    player.update();
+  }
+}}
